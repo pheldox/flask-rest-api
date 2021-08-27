4ecc84a50b22ebe8bb8ba524078bdf8feb9f53e2
pheldox <felixaryee@gmail.com>
2021-08-27 09:26:30 +0000

    indentation

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ api.add_resource(StoreList, '/stores')
 
 
 if __name__ == '__main__':
-        from db import db 
-        db.init_app(app)
-        app.run(port=5000, debug=True)
+    from db import db 
+    db.init_app(app)
+    app.run(port=5000, debug=True)
